d085a534cc56a10cfb2b765fa833861c0cdd06f9
frogggggi <frogggggi@gmail.com>
2018-02-06 00:16:04 +0200

    some changes in templates and add fixtures named initial_data.json

--- tt1/settings.py
+++ tt1/settings.py
@@ -128,6 +128,10 @@ _PATH = os.path.abspath(os.path.dirname(__file__))
 MEDIA_ROOT = os.path.join(_PATH, 'files', 'media')
 MEDIA_URL = '/media/'
 
+FIXTURE_DIRS = (
+   '/fixtures/',
+)
+
 STATIC_ROOT = os.path.join(_PATH, 'files', 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
