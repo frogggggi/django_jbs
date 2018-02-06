6d90a8e89ea70d9891826d0a552f991101a1af77
frogggggi <frogggggi@gmail.com>
2018-02-06 19:03:17 +0200

    create a tasks.py file using celery

--- new_tt1/tasks.py
+++ new_tt1/tasks.py
@@ -1,6 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 from celery.task import periodic_task
 from datetime import timedelta
+# from celery.schedules import crontab
 
-@periodic_task(run_every = timedelta(seconds = 60))
+# используя селери записывать какое то значение в celery.log
+@periodic_task(run_every = timedelta(seconds = 120))
 def test():
     print "is works!"
+    # открыть файл на дозапись
+    # запускаем командой ./manage.py celeryd -l INFO -B
+    f = open("celery.log", "a")
+    f.write('celery is works! ' + '\n')
+    f.close()
